01f7a0aa7a6a9c83be0d2476a2a75181e57add9c
adcpm <fabien@bonustrack.co>
2020-06-11 03:48:41 +0700

    Add abis

---
+++ src/helpers/abi/index.ts
@@ -0,0 +1,23 @@
+import { abi as BPool } from '@/helpers/abi/BPool.json';
+import { abi as BActions } from '@/helpers/abi/BActions.json';
+import { abi as BFactory } from '@/helpers/abi/BFactory.json';
+import { abi as DSProxy } from '@/helpers/abi/DSProxy.json';
+import { abi as DSProxyRegistry } from '@/helpers/abi/DSProxyRegistry.json';
+import { abi as TestToken } from '@/helpers/abi/TestToken.json';
+import { abi as ExchangeProxy } from '@/helpers/abi/ExchangeProxy.json';
+import { abi as ExchangeProxyCallable } from '@/helpers/abi/ExchangeProxyCallable.json';
+import { abi as Weth } from '@/helpers/abi/Weth.json';
+import { abi as Multicall } from '@/helpers/abi/Multicall.json';
+
+export default {
+  BPool,
+  BActions,
+  BFactory,
+  DSProxy,
+  DSProxyRegistry,
+  TestToken,
+  ExchangeProxy,
+  ExchangeProxyCallable,
+  Weth,
+  Multicall
+};
--- src/helpers/provider.ts
+++ src/helpers/provider.ts
@@ -1,4 +1,5 @@
 import { ethers } from 'ethers';
+import abi from '@/helpers/abi';
 
 let provider;
 
@@ -7,5 +8,9 @@ if (typeof window.ethereum !== 'undefined') {
   const ethereum = window['ethereum'];
   provider = new ethers.providers.Web3Provider(ethereum);
 }
+
+provider.getContract = (type: string, address: string): ethers.Contract => {
+  return new ethers.Contract(address, abi[type], provider);
+};
 
 export default provider;
