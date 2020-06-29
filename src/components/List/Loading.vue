7100d45c6711b40b426ab08ea174313ac22661c8
adcpm <fabien@bonustrack.co>
2020-06-30 00:08:54 +0700

    Fix lint

--- src/components/List/Loading.vue
+++ src/components/List/Loading.vue
@@ -6,7 +6,12 @@
       class="py-3 border-top d-flex text-right v-align-middle px-4"
       style="line-height: 0;"
     >
-      <div v-for="(itemClass, i2) in classes" :class="itemClass" :key="i2" :style="`opacity: ${1 - (i - 1) / 2.5}`">
+      <div
+        v-for="(itemClass, i2) in classes"
+        :class="itemClass"
+        :key="i2"
+        :style="`opacity: ${1 - (i - 1) / 2.5}`"
+      >
         <span
           class="bg-gray d-inline-block rounded-1 anim-pulse my-1"
           style="height: 30px; width: 40%;"
