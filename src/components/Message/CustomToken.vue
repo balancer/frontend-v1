8b34b80acf821d2dfe49f7c7897e57748622685c
Timur Badretdinov <destinerx@gmail.com>
2020-07-22 14:43:16 +0300

    Add CustomToken component

---
+++ src/components/Message/CustomToken.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="error" class="d-flex flex-items-center px-4 py-2">
+    <div>
+      <UiCheckbox :checked="accepted" @change="$emit('toggle')" />
+    </div>
+    <div class="ml-4">
+      <div>
+        • Do not add <b>deflationary tokens</b> or tokens with transfer fees.
+      </div>
+      <div>• Do not add tokens with <b>no bool return values</b>.</div>
+      <div>
+        • Any other <b>non-compliance from ERC20</b> may cause issues. DYOR!
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      accepted: false
+    };
+  }
+};
+</script>
+
+<style scoped lang="scss">
+@import '../../vars';
+
+#error {
+  border: 1px solid $error;
+  border-radius: 4px;
+  color: $error;
+}
+</style>
