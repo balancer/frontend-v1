67916fd13e396ece00861d2dd9330bf7a7b8f961
Timur Badretdinov <destinerx@gmail.com>
2020-07-23 00:07:54 +0300

    Add MessageWarning component

---
+++ src/components/Message/Warning.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="warning" class="p-4 text-left">
+    <Icon name="warning" class="mr-4" />
+    {{ text }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['text']
+};
+</script>
+
+<style scoped lang="scss">
+@import '../../vars';
+
+#warning {
+  border: 1px solid $warning;
+  border-radius: 4px;
+  color: $warning;
+}
+</style>
