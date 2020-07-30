ba034ac037e18c72ab08e01a2a8780f6cb53d02d
Fabien <bonustrack@users.noreply.github.com>
2020-07-30 14:34:26 +0700

    Add Coinbase login (#61)

--- src/helpers/lock.ts
+++ src/helpers/lock.ts
@@ -3,8 +3,9 @@ import config from '@/helpers/config';
 import injected from '@bonustrack/lock/connectors/injected';
 import portis from '@bonustrack/lock/connectors/portis';
 import walletconnect from '@bonustrack/lock/connectors/walletconnect';
+import walletlink from '@bonustrack/lock/connectors/walletlink';
 
-const connectors = { injected, portis, walletconnect };
+const connectors = { injected, portis, walletconnect, walletlink };
 const lock = new Lock();
 Object.entries(config.connectors).forEach((connector: any) => {
   lock.addConnector({
