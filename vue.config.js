b277824c8132107abe681d3a9a05a28b1e6c437e
adcpm <fabien@bonustrack.co>
2020-08-01 23:46:18 +0700

    Remove bn.js duplicates

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   pluginOptions: {
     express: {
@@ -7,5 +9,11 @@ module.exports = {
     webpackBundleAnalyzer: {
       openAnalyzer: false
     }
+  },
+  chainWebpack: config => {
+    config.resolve.alias.set(
+      'bn.js',
+      path.resolve(path.join(__dirname, 'node_modules', 'bn.js'))
+    );
   }
 };
