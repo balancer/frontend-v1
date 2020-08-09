de713d92ede510752a881cff4756d48063a68263
Timur Badretdinov <destinerx@gmail.com>
2020-08-09 15:52:18 +0300

    UX improvements (#83)
    * Add loading state to button in Create view
    
    * Redirect to Setup view if no proxy found
    
    * Add loading state to Wrap/Unwrap buttons
    
    * Add validation for Wrapper inputs
    
    * Hide pool action buttons when web3 not connected
    
    * Add message to Checkbox
    
    * Bump @walletconnect/web3-provider
    
    * Hide MessageSimilarPools for private pools
    
    * Add $info style var
    
    * Add Slippage message
    
    * Cut Pie from PoolOverview
    
    * Add Icon to Slippage message
    
    * Decrease vertical margin for several messages
    
    * Increase max-height for UiModal
    
    * Move messages inside Add/Remove modal body

--- src/components/Form/Wrapper.vue
+++ src/components/Form/Wrapper.vue
@@ -4,12 +4,18 @@
     <div class="d-flex mb-3">
       <div class="input d-flex flex-justify-end">
         <input
-          v-model="weth.wrapAmount"
-          class="flex-auto text-right text-white amount-input"
+          v-model="wrapAmount"
+          class="flex-auto text-right amount-input"
+          :class="wrapInputValid ? 'text-white' : 'text-red'"
           placeholder="0.0"
         />
       </div>
-      <UiButton class="ml-2 px-4" @click="wrapEther">
+      <UiButton
+        class="ml-2 px-4"
+        @click="wrapEther"
+        :loading="wrapLoading"
+        :disabled="!wrapInputValid"
+      >
         Wrap
       </UiButton>
     </div>
@@ -24,12 +30,18 @@
           <UiLabel v-text="'Max'" />
         </a>
         <input
-          v-model="weth.unwrapAmount"
-          class="flex-auto text-right text-white amount-input ml-1"
+          v-model="unwrapAmount"
+          class="flex-auto text-right amount-input ml-1"
+          :class="unwrapInputValid ? 'text-white' : 'text-red'"
           placeholder="0.0"
         />
       </div>
-      <UiButton class="ml-2 px-3" @click="unwrapEther">
+      <UiButton
+        class="ml-2 px-3"
+        @click="unwrapEther"
+        :loading="unwrapLoading"
+        :disabled="!unwrapInputValid"
+      >
         Unwrap
       </UiButton>
     </div>
@@ -39,29 +51,60 @@
 <script>
 import { mapActions } from 'vuex';
 import { normalizeBalance } from '@/helpers/utils';
+import { validateNumberInput, ValidationError } from '@/helpers/validation';
 
 export default {
   data() {
     return {
-      weth: {
-        wrapAmount: '',
-        unwrapAmount: ''
-      }
+      wrapAmount: '',
+      wrapLoading: false,
+      unwrapAmount: '',
+      unwrapLoading: false
     };
+  },
+  computed: {
+    wrapInputValid() {
+      const error = validateNumberInput(this.wrapAmount);
+      if (error !== ValidationError.NONE && error !== ValidationError.EMPTY) {
+        return false;
+      }
+      const ethBalance = this.web3.balances['ether'] || '0';
+      const balance = normalizeBalance(ethBalance, 18);
+      if (balance.lt(this.wrapAmount)) {
+        return false;
+      }
+      return true;
+    },
+    unwrapInputValid() {
+      const error = validateNumberInput(this.unwrapAmount);
+      if (error !== ValidationError.NONE && error !== ValidationError.EMPTY) {
+        return false;
+      }
+      const wethBalance = this.web3.balances[this.config.addresses.weth] || '0';
+      const balance = normalizeBalance(wethBalance, 18);
+      if (balance.lt(this.unwrapAmount)) {
+        return false;
+      }
+      return true;
+    }
   },
   methods: {
     ...mapActions(['wrap', 'unwrap']),
-    wrapEther() {
-      this.wrap(this.weth.wrapAmount);
+    async wrapEther() {
+      this.wrapLoading = true;
+      await this.wrap(this.wrapAmount);
+      this.wrapLoading = false;
     },
-    unwrapEther() {
-      this.unwrap(this.weth.unwrapAmount);
+    async unwrapEther() {
+      this.unwrapLoading = true;
+      await this.unwrap(this.unwrapAmount);
+      this.unwrapLoading = false;
     },
     handleMax() {
       const weth = this.config.tokens[this.config.addresses.weth];
       const wethBalance = this.web3.balances[weth.address];
       const balance = normalizeBalance(wethBalance, weth.decimals);
-      this.weth.unwrapAmount = balance.toString();
+      this.unwrapAmount = balance.toString();
     }
   }
 };
