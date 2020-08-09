b5d905086b53ed8092b8ba9bb0b403b791d1c466
Timur Badretdinov <destinerx@gmail.com>
2020-08-09 23:49:40 +0300

    Fix getTokenLogoUrl for custom tokens

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -156,7 +156,8 @@ export function getTokenLogoUrl(address: string): string | null {
     trustwalletId = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2';
   } else {
     const checksum = getAddress(address);
-    if (config.tokens[checksum].hasIcon) {
+    const token = config.tokens[checksum];
+    if (token && token.hasIcon) {
       trustwalletId = checksum;
     }
   }
