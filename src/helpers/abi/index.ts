af0dd63593004ec9d9a28ef2f2eaa5c7d80947eb
Timur Badretdinov <destinerx@gmail.com>
2020-08-18 15:47:43 +0300

    Add ConfigurableRightsPool ABI

--- src/helpers/abi/index.ts
+++ src/helpers/abi/index.ts
@@ -1,6 +1,7 @@
 import { abi as BPool } from '@/helpers/abi/BPool.json';
 import { abi as BActions } from '@/helpers/abi/BActions.json';
 import { abi as BFactory } from '@/helpers/abi/BFactory.json';
+import { abi as ConfigurableRightsPool } from '@/helpers/abi/ConfigurableRightsPool.json';
 import { abi as CRPFactory } from '@/helpers/abi/CRPFactory.json';
 import { abi as DSProxy } from '@/helpers/abi/DSProxy.json';
 import { abi as DSProxyRegistry } from '@/helpers/abi/DSProxyRegistry.json';
@@ -14,6 +15,7 @@ export default {
   BPool,
   BActions,
   BFactory,
+  ConfigurableRightsPool,
   CRPFactory,
   DSProxy,
   DSProxyRegistry,
