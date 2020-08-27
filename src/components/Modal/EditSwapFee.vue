3de52eb830e5621c8c011d9f4aed4c8871e18c80
bonustrack <fabien@bonustrack.co>
2020-08-27 19:21:46 +0700

    Add edit swap fee modal

--- src/components/Modal/EditSwapFee.vue
+++ src/components/Modal/EditSwapFee.vue
@@ -1,80 +1,81 @@
 <template>
-  <UiModal :open="open" @close="$emit('close')">
-    <div
-      class="modal-body py-6 text-center"
-      :class="{ 'bg-blue mosaic anim-scroll': step > lastStep }"
-    >
-      <form @submit.prevent="handleSubmit">
-        <div v-if="step === 0">
-          <div class="px-4 mb-4 overflow-hidden">
-            <h2 class="mb-3">Swap fee</h2>
-            <p class="mb-3">
-              Excepteur sint occaecat cupidatat non proident, sunt in.
-            </p>
-          </div>
-          <div class="mb-4">
-            <label class="d-block">Swap fee (%)</label>
-            <input
-              type="number"
-              class="h1 border-0 form-control width-fit text-center"
-              placeholder="0.0001"
-              value="0.15"
-              step="0.0001"
-              min="0.0001"
-              max="10"
-              v-model="swapFee"
-            />
-          </div>
-          <div class="mx-3 overflow-hidden">
-            <button
-              type="button"
-              class="btn-outline d-inline-block column mx-1"
-              @click="$emit('close')"
-            >
-              Cancel
-            </button>
-            <button
-              :disabled="loading || swapFee === value"
-              type="submit"
-              class="btn-mktg d-inline-block column mx-1"
-            >
-              Confirm
-            </button>
-          </div>
-        </div>
-        <FormBroadcast v-if="step === 1" @close="$emit('close')" />
-      </form>
-    </div>
+  <UiModal :open="open" @close="$emit('close')" style="max-width: 440px;">
+    <UiModalForm @submit="handleSubmit">
+      <template slot="header">
+        <h3 class="text-white">Edit swap fee</h3>
+      </template>
+      <div class="text-center m-4">
+        <h5 class="px-4 mb-4 mx-auto overflow-hidden" style="max-width: 340px;">
+          You can set a swap fee from 0.0001% to maximum 10%.
+        </h5>
+        <input
+          type="number"
+          class="h3 py-2 px-3 input text-center"
+          placeholder="0.0001"
+          :class="isValid ? 'text-white' : 'text-red'"
+          value="0.15"
+          step="0.0001"
+          min="0.0001"
+          max="10"
+          v-model="input"
+        />
+      </div>
+      <template slot="footer">
+        <UiButton @click="$emit('close')" type="button" class="mx-1">
+          Cancel
+        </UiButton>
+        <UiButton
+          :disabled="loading || !isValid || input === value"
+          :loading="loading"
+          type="submit"
+          class="button-primary mx-1"
+        >
+          Confirm
+        </UiButton>
+      </template>
+    </UiModalForm>
   </UiModal>
 </template>
 
 <script>
-import { delay, clone } from '@/helpers/utils';
+import { mapActions } from 'vuex';
+import { clone } from '@/helpers/utils';
 
 export default {
-  props: ['open', 'value'],
+  props: ['open', 'value', 'pool'],
   data() {
     return {
       loading: false,
-      step: 0,
-      lastStep: 0,
-      swapFee: ''
+      tx: null,
+      input: ''
     };
   },
   watch: {
     open() {
-      this.step = 0;
       this.loading = false;
-      this.swapFee = clone(this.value);
+      this.input = clone(this.value);
+    }
+  },
+  computed: {
+    isValid() {
+      const swapFee = parseFloat(this.input);
+      return !(swapFee < 0.0001 || swapFee > 10);
     }
   },
   methods: {
+    ...mapActions(['setSwapFee']),
     async handleSubmit() {
       this.loading = true;
-      await delay(1e3);
-      // @TODO Broadcast tx
+      try {
+        this.tx = await this.setSwapFee({
+          poolAddress: this.pool.controller,
+          newFee: this.input
+        });
+        this.$emit('close');
+      } catch (e) {
+        console.error(e);
+      }
       this.loading = false;
-      this.step++;
     }
   }
 };
