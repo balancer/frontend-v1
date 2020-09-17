9016b24e0d335c2b607c1ba5cfd02ea4456aa175
Timur Badretdinov <destinerx@gmail.com>
2020-09-17 10:23:58 +0000

    Add cap, controller, weights to Settings tab

--- src/views/Pool/Settings.vue
+++ src/views/Pool/Settings.vue
@@ -2,14 +2,28 @@
   <div class="border rounded-1 panel-background p-4 text-white">
     <div class="border-bottom mb-4 pb-3">
       <div class="float-right">
-        <UiButton @click="modalEditPublicSwapOpen = true">Edit</UiButton>
+        <UiButton @click="modalOpen.publicSwap = true">Toggle</UiButton>
       </div>
       <label class="d-block mb-2">Public swap</label>
       <p v-text="pool.publicSwap ? 'Active' : 'Paused'" class="text-gray" />
     </div>
     <div class="border-bottom mb-4 pb-3">
       <div class="float-right">
-        <UiButton @click="modalEditTokensOpen = true">Edit</UiButton>
+        <UiButton @click="modalOpen.swapFee = true">Change</UiButton>
+      </div>
+      <label class="d-block mb-2">Swap fee</label>
+      <p v-text="_num(pool.swapFee, 'percent')" class="text-gray" />
+    </div>
+    <div class="border-bottom mb-4 pb-3">
+      <div class="float-right">
+        <UiButton @click="modalOpen.cap = true">Change</UiButton>
+      </div>
+      <label class="d-block mb-2">Cap</label>
+      <p v-text="_num(cap)" class="text-gray" />
+    </div>
+    <div class="border-bottom mb-4 pb-3">
+      <div class="float-right">
+        <UiButton @click="modalOpen.tokens = true">Add</UiButton>
       </div>
       <label class="d-block mb-2">Tokens</label>
       <div v-if="pool.tokens.length" class="mb-2">
@@ -30,53 +44,93 @@
     </div>
     <div class="border-bottom mb-4 pb-3">
       <div class="float-right">
-        <UiButton @click="modalEditWeightsOpen = true">Edit</UiButton>
+        <UiButton @click="modalOpen.gradualWeights = true">Update gradually</UiButton>
+      </div>
+      <div class="float-right mr-2">
+        <UiButton @click="modalOpen.weights = true">Update</UiButton>
       </div>
       <label class="d-block mb-2">Weights</label>
       <Pie :tokens="pool.tokens" size="64" class="mr-2" />
     </div>
     <div class="pb-3">
       <div class="float-right">
-        <UiButton @click="modalEditSwapFeeOpen = true">Edit</UiButton>
+        <UiButton @click="modalOpen.controller = true">Change</UiButton>
       </div>
-      <label class="d-block mb-2">Swap fee</label>
-      <p v-text="_num(pool.swapFee, 'percent')" class="text-gray" />
+      <label class="d-block mb-2">Controller</label>
+      <p v-text="pool.crpController" class="text-gray" />
     </div>
     <ModalEditPublicSwap
       :pool="pool"
       :value="pool.publicSwap"
-      :open="modalEditPublicSwapOpen"
-      @close="modalEditPublicSwapOpen = false"
-    />
-    <ModalEditTokens
-      :value="pool.tokensList"
-      :open="modalEditTokensOpen"
-      @close="modalEditTokensOpen = false"
-    />
-    <ModalEditWeights
-      :pool="pool"
-      :open="modalEditWeightsOpen"
-      @close="modalEditWeightsOpen = false"
+      :open="modalOpen.publicSwap"
+      @close="modalOpen.publicSwap = false"
     />
     <ModalEditSwapFee
       :pool="pool"
       :value="pool.swapFee * 1e2"
-      :open="modalEditSwapFeeOpen"
-      @close="modalEditSwapFeeOpen = false"
+      :open="modalOpen.swapFee"
+      @close="modalOpen.swapFee = false"
+    />
+    <ModalEditController
+      :value="pool.crpController"
+      :pool="pool"
+      :open="modalOpen.controller"
+      @close="modalOpen.controller = false"
+    />
+    <ModalEditCap
+      :value="cap"
+      :pool="pool"
+      :open="modalOpen.cap"
+      @close="modalOpen.cap = false"
+    />
+    <ModalEditWeights
+      :pool="pool"
+      :open="modalOpen.weights"
+      @close="modalOpen.weights = false"
+    />
+    <ModalEditWeightsGradually
+      :pool="pool"
+      :open="modalOpen.gradualWeights"
+      @close="modalOpen.gradualWeights = false"
+    />
+    <ModalEditTokens
+      :value="pool.tokensList"
+      :open="modalOpen.tokens"
+      @close="modalOpen.tokens = false"
     />
   </div>
 </template>
 
 <script>
+import { getAddress } from '@ethersproject/address';
+
+import { bnum, scale } from '@/helpers/utils';
+
 export default {
   props: ['pool'],
   data() {
     return {
-      modalEditPublicSwapOpen: false,
-      modalEditTokensOpen: false,
-      modalEditSwapFeeOpen: false,
-      modalEditWeightsOpen: false
+      modalOpen: {
+        swapFee: false,
+        publicSwap: false,
+        controller: false,
+        cap: false,
+        weights: false,
+        gradualWeights: false,
+        tokens: false
+      }
     };
-  }
+  },
+  computed: {
+    cap() {
+      const address = getAddress(this.pool.controller);
+      const crp = this.web3.crps[address];
+      if (!crp) {
+        return '0';
+      }
+      const capNumber = scale(bnum(crp.bspCap), -18);
+      return capNumber.toString();
+    },
+  },
 };
 </script>
