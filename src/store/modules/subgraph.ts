43614cc4f91b5f2c381ece62af1118ba08079530
Timur Badretdinov <destinerx@gmail.com>
2020-09-24 17:36:59 +0300

    Sync user state & pool balances (#142)
    * Clear user subgraph data on account change
    
    * Refactor: getMyPools -> getUserPools etc
    
    * Fetch ETH/WETH balance after wrapping/unwrapping
    
    * Sync balances/shares in background
    
    * Fetch pool balances onchain

--- src/store/modules/subgraph.ts
+++ src/store/modules/subgraph.ts
@@ -104,6 +104,9 @@ const mutations = {
 };
 
 const actions = {
+  clearUser: async ({ commit }) => {
+    commit('CLEAR_USER');
+  },
   getBalancer: async ({ commit }) => {
     commit('GET_BALANCER_REQUEST');
     try {
@@ -185,7 +188,7 @@ const actions = {
       commit('GET_POOLS_FAILURE', e);
     }
   },
-  getMyPools: async ({ commit }) => {
+  getUserPools: async ({ commit }) => {
     commit('GET_MY_POOLS_REQUEST');
     try {
       const myPools = [
@@ -202,7 +205,7 @@ const actions = {
       commit('GET_MY_POOLS_FAILURE', e);
     }
   },
-  getMyPoolShares: async ({ commit, rootState }) => {
+  getUserPoolShares: async ({ commit, rootState }) => {
     const address = rootState.web3.account;
     commit('GET_MY_POOLS_SHARES_REQUEST');
     try {
@@ -215,7 +218,7 @@ const actions = {
           }
         }
       };
-      const { poolShares } = await request('getMyPoolShares', query);
+      const { poolShares } = await request('getUserPoolShares', query);
       const balances: any = {};
       poolShares.forEach(share => (balances[share.poolId.id] = share.balance));
       commit('GET_MY_POOLS_SHARES_SUCCESS', balances);
