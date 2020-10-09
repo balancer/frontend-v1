f16fe50c385496d4e76acbd128a813dbad4de2fe
bonustrack <fabien@bonustrack.co>
2020-10-09 19:45:07 +0700

    Fix spacing

--- src/components/PoolOverview.vue
+++ src/components/PoolOverview.vue
@@ -3,29 +3,26 @@
     class="p-4 border-left-0 border-right-0 border-md border rounded-0 rounded-md-1 panel-background"
   >
     <div class="eyebrow mb-4">
-      Pool Overview
+      Pool overview
     </div>
     <div class="text-white">
       {{ _shortenAddress(pool.id) }}
     </div>
     <div class="text-white">
       My share: {{ _num(userShare.current, 'percent') }}
-      <span v-if="userShare.future">
-        → {{ _num(userShare.future, 'percent') }}
-      </span>
+      <span
+        v-if="userShare.future"
+        v-text="`→ ${_num(userShare.future, 'percent')}`"
+      />
     </div>
     <div class="text-white">Swap fee: {{ _num(pool.swapFee, 'percent') }}</div>
     <div class="text-center">
-      <Pie :tokens="pool.tokens" size="100" class="my-3" />
+      <Pie :tokens="pool.tokens" size="100" class="mt-3 mb-2" />
     </div>
-    <div
-      class="mt-2 d-flex flex-column flex-items-center flex-justify-center text-white"
-    >
-      <div v-for="token in pool.tokens" :key="token.address" class="mt-1">
-        <Icon name="bullet" size="16" :style="`color: ${token.color}`" />
-        {{ _num(token.weightPercent / 100, 'percent') }}
-        {{ _ticker(token.checksum) }}
-      </div>
+    <div v-for="token in pool.tokens" :key="token.address" class="mt-1 text-white">
+      <Icon name="bullet" size="16" :style="`color: ${token.color}`" />
+      {{ _num(token.weightPercent / 100, 'percent') }}
+      {{ _ticker(token.checksum) }}
     </div>
   </div>
 </template>
