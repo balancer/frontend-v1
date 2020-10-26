ad47323e7b058faba0346d5e40eaffb9f89567a6
Timur Badretdinov <destinerx@gmail.com>
2020-10-26 19:04:13 +0300

    Add gas buffer for eth wrapping + add a warning for wrapping (#180)
    * Add gas buffer for eth wrapping
    
    * Disable wrap button if leftover eth is <0.01

--- src/components/Modal/Wrapper.vue
+++ src/components/Modal/Wrapper.vue
@@ -48,6 +48,9 @@
           />
           {{ symbols.tokenOut }}
         </UiButton>
+        <div class="text-yellow text-center mt-3" v-if="!etherLeft">
+          Keep some ether to pay for gas.
+        </div>
       </div>
       <template slot="footer">
         <UiButton @click="$emit('close')" type="button" class="mx-1">
@@ -70,6 +73,9 @@
 import { validateNumberInput, ValidationError } from '@/helpers/validation';
 import { normalizeBalance } from '@/helpers/utils';
 import { mapActions } from 'vuex';
+
+const GAS_BUFFER_ERROR = 0.01;
+const GAS_BUFFER_WARNING = 0.2;
 
 export default {
   props: ['open', 'side'],
@@ -105,9 +111,16 @@ export default {
     },
     isValid() {
       const error = validateNumberInput(this.amount);
-      if (error !== ValidationError.NONE && error !== ValidationError.EMPTY)
-        return false;
-      return !this.balance.lt(this.amount);
+      if (error !== ValidationError.NONE) return false;
+      return this.currentSide === 1
+        ? !this.balance.minus(GAS_BUFFER_ERROR).lt(this.amount)
+        : !this.balance.lt(this.amount);
+    },
+    etherLeft() {
+      return (
+        this.currentSide === 2 ||
+        !this.balance.minus(GAS_BUFFER_WARNING).lt(this.amount)
+      );
     }
   },
   methods: {
@@ -120,7 +133,10 @@ export default {
       this.$emit('close');
     },
     handleMax() {
-      this.amount = this.balance.toString();
+      this.amount =
+        this.currentSide === 1
+          ? this.balance.minus(GAS_BUFFER_WARNING).toString()
+          : this.balance.toString();
     },
     toggleSide() {
       this.currentSide = this.currentSide === 1 ? 2 : 1;
