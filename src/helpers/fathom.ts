84f2b9223be4097c15baefc940fc281b9334e9f6
bonustrack <fabien@bonustrack.co>
2020-11-03 20:56:41 +0700

    Load script from Vue

--- src/helpers/fathom.ts
+++ src/helpers/fathom.ts
@@ -1,5 +1,15 @@
 import config from '@/config';
 
+if (config.network === 'homestead') {
+  const script = document.createElement('script');
+  script.setAttribute('src', 'https://cdn.usefathom.com/script.js');
+  script.setAttribute('data-spa', 'auto');
+  script.setAttribute('data-site', 'LANWVFKK');
+  script.setAttribute('honor-dnt', 'true');
+  script.setAttribute('defer', '');
+  document.head.appendChild(script);
+}
+
 export function setGoal(id, value = 0) {
   if (window['fathom'] && config.network === 'homestead')
     window['fathom'].trackGoal(id, value);
