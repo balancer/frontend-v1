c7284469105e55ff11c9e79db37de616690bd61c
Fabien <bonustrack@users.noreply.github.com>
2020-11-03 22:39:47 +0700

    Shorten long symbols (#195)

--- src/components/Modal/AddLiquidity.vue
+++ src/components/Modal/AddLiquidity.vue
@@ -47,7 +47,7 @@
                   "
                 />
                 <Token :address="token.address" class="mr-2" size="20" />
-                <div v-text="token.symbol" class="text-white" />
+                <div v-text="_shorten(token.symbol, 12)" class="text-white" />
                 <ButtonUnlock
                   class="button-primary ml-2"
                   :tokenAddress="token.checksum"
