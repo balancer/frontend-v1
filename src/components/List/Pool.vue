1b2f49d6ea2ae5d0fd36034493370af0faf407fb
bonustrack <fabien@bonustrack.co>
2020-11-26 18:23:49 +0700

    Add tooltip on long symbol name

--- src/components/List/Pool.vue
+++ src/components/List/Pool.vue
@@ -7,15 +7,17 @@
       <Pie :tokens="pool.tokens" class="mr-3" size="34" />
     </div>
     <div class="flex-auto text-left">
-      <div class="d-flex flex-wrap overflow-hidden" style="max-width: 340px;">
+      <div class="d-flex flex-wrap" style="max-width: 340px;">
         <div
           v-for="token in pool.tokens"
           :key="token.address"
+          :class="token.symbol.length > 14 && 'tooltipped tooltipped-n'"
+          :aria-label="token.symbol"
           class="d-flex flex-items-center mr-2"
         >
           <Icon name="bullet" size="16" :style="`color: ${token.color}`" />
           {{ _num(token.weightPercent / 100, 'percent-short') }}
-          {{ _shorten(token.symbol, 12) }}
+          {{ _shorten(token.symbol, 14) }}
         </div>
       </div>
     </div>
