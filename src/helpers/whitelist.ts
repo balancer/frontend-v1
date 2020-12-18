2f7f02e70703e1d33d825b3a71d98b3862785e87
EndymionJkb <EndymionJkb@gmail.com>
2020-12-18 10:56:08 -0500

    Adding liquidity to smart pools (#229)
    * WIP - Disable adding liquidity to smart pools with whitelist enabled (call to contract to check current address not yet working)
    
    * Lint
    
    * Simplify call to canProvideLiquidity
    
    * lint
    
    * Check the proxy address instead of the wallet address (the proxy will be the caller)

---
+++ src/helpers/whitelist.ts
@@ -0,0 +1,12 @@
+import { Contract } from '@ethersproject/contracts';
+import provider from '@/helpers/provider';
+import abi from '@/helpers/abi';
+
+export async function canProvideLiquidity(crpAddress, walletAddress) {
+  const crpContract = new Contract(
+    crpAddress,
+    abi['ConfigurableRightsPool'],
+    provider
+  );
+  return await crpContract.canProvideLiquidity(walletAddress);
+}
