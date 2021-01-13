af858f9f0493c531ca86eea70b64decba135943d
EndymionJkb <EndymionJkb@gmail.com>
2021-01-13 15:56:49 -0500

    Add year to gradual update notices (per user request) (#241)

--- src/views/Pool/About.vue
+++ src/views/Pool/About.vue
@@ -257,6 +257,7 @@ export default {
       return new Date(blockTimestamp).toLocaleString('en-US', {
         month: 'long',
         day: 'numeric',
+        year: 'numeric',
         hour: '2-digit',
         minute: '2-digit'
       });
